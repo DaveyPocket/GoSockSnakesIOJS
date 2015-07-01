4381dcfdee7647aa71822cb26a13ed6bcb34272b
Bradley Boccuzzi <bjboccuzzi@gmail.com>
2015-07-01 16:33:00 -0400

    Server changes

--- server.go
+++ server.go
@@ -8,43 +8,81 @@ import (
 	"time"
 	"./game"
 )
+type mserve struct {
+	list	[]users
+}
+type users struct {
+	sock			socketio.Socket
+	clientID		int
+}
 
 func main() {
+	var pause bool
+	clients := 0
+	m := new(mserve)
+	m.list = make([]users, 10)
 	g := logic.InitGame()
-	g.AddSnake(20, 20, 5, "LEFT")
-//	g.AddSnake(2, 3, 8, "RIGHT")
     server, err := socketio.NewServer(nil)
     if err != nil {
         log.Fatal(err)
     }
-    server.On("connection", func(so socketio.Socket) {
 	
-        log.Println("on connection")
+	// Make functions (Could this be done differently???)
+	readyFunc := func (msg string) {
+		log.Println("Received ready", msg)
+		pause = false
+/*		go func() {
+			for {
+				log.Println("tick")
+				so.Emit("tick")
+				time.Sleep(500 * time.Millisecond)
+			}
+		}(v)*/
+	}
+
+
+	connFunc := func(so socketio.Socket) {	
+        log.Println("Client connected")
+		pause = true
         so.Join("main")
+		m.list[clients] = users{so, clients}
+		clients++
+		log.Println(&m.list)
         so.On("join game", func(msg string) {
+			lcl := clients
+			g.AddSnake(20, 20+clients, 5, "LEFT")
             log.Println("Received join game", msg)
-            so.Emit("init setup", g)
-			log.Println("Finished broadcast")
+			sp := logic.GetPacket(*g, lcl - 1)
+			so.Emit("init setup", sp)
+			so.BroadcastTo("main", "init setup", sp)
+			if clients == 1 {
+				log.Println("Start ticker")
+				go func() {
+					for {
+						if pause == false {
+							log.Println("tick")
+							so.Emit("tick")
+							so.BroadcastTo("main", "tick")
+							g.Tick()
+						}
+						time.Sleep(200 * time.Millisecond)
+					}
+				}()
+			}
         })
-		so.On("yes", func(){
-			log.Println("Hi")
-		})
-		so.On("ready", func(msg string) {
+		so.On("ready", readyFunc)
+		so.On("change", func(cmd string){
+			sp := logic.GetPacket(*g, clients - 1)
+			g.Snake[m.getId(so)].Dir = cmd
+			so.Emit("init setup", sp)
+			so.BroadcastTo("main", "init setup", sp)
 
-			log.Println("Received ready", msg)
-
-			go func() {
-				for {
-					log.Println("tick")
-					so.Emit("tick")
-					time.Sleep(500 * time.Millisecond)
-				}}()
 		})
-	so.On("disconnection", func() {
-            log.Println("on disconnect")
-        })
-    })
-    server.On("error", func(so socketio.Socket, err error) {
+		so.On("disconnection", disConnFunc)
+	}
+
+	server.On("connection", connFunc)
+	server.On("error", func(so socketio.Socket, err error) {
         log.Println("error:", err)
     })
 
@@ -55,10 +93,15 @@ func main() {
     log.Fatal(http.ListenAndServe(":5000", nil))
 }
 
-func servTick(so socketio.Socket) {
-	for {
-		so.BroadcastTo("main", "tick")
-		time.Sleep(500 * time.Millisecond)
-	}
+func disConnFunc(so socketio.Socket) {
+	log.Println("on disconnect", so)
+}
 
+func (m* mserve) getId(sock socketio.Socket) (int) {
+	for _, u := range m.list {
+		if u.sock == sock {
+			return u.clientID
+		}
+	} 
+	return -1
 }
